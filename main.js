89c18e53ada785f114e215550b1da4e58f38b07a
Tom Gooden <good3n@gmail.com>
2021-09-25 14:07:44 -0400

    Create example page

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 import "./src/styles/style.css";
+import "./src/example.css";
 
 document.querySelector("#app").innerHTML = `
-  <h1 style="font-size:var(--fontSize__h0);">Great Lakes Psychology Group</h1>
-  <h1>Great Lakes Psychology Group</h1>
-  <h2>Great Lakes Psychology Group</h2>
-  <h3>Great Lakes Psychology Group</h3>
-  <h4>Great Lakes Psychology Group</h4>
-  <h5>Great Lakes Psychology Group</h5>
-  <p>At GLPG, we value helping our clients find the right therapist for them. Therapy is not one-size-fits-all, which is why our clinical staff consists of doctoral and master’s level psychologists, social workers, counselors, and marriage and family therapists to fit a wide range of problem areas.</p>
-  <p>Our licensed professionals make knowledgeable assessments and provide evidence-based treatments to help those struggling with depression, anxiety, relationship issues, grief, stress, eating disorders, abuse, chronic pain, PTSD, ADD/ADHD, and much more. With a diverse team of specialists, we are committing to helping our clients find the perfect fit.</p>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <div class="container">
+    <h1 style="font-size:var(--fontSize__h0);">Great Lakes Psychology Group</h1>
+    <h1>Great Lakes Psychology Group</h1>
+    <h2>Great Lakes Psychology Group</h2>
+    <h3>Great Lakes Psychology Group</h3>
+    <h4>Great Lakes Psychology Group</h4>
+    <h5>Great Lakes Psychology Group</h5>
+    <p>At GLPG, we value helping our clients find the right therapist for them. Therapy is not <a href="#">one-size-fits-all</a>, which is why our clinical staff consists of doctoral and master’s level psychologists, social workers, counselors, and marriage and family therapists to fit a wide range of problem areas.</p>
+    <p>Our licensed professionals make knowledgeable <strong>assessments and provide</strong> evidence-based treatments to help those struggling with depression, anxiety, relationship issues, grief, stress, eating disorders, abuse, chronic pain, PTSD, ADD/ADHD, and much more. With a diverse team of specialists, we are committing to helping our clients find the perfect fit.</p>
+    <ul>
+      <li>Example list item</li>
+      <li>Example list item</li>
+      <li>Example list item</li>
+      <li>Example list item</li>
+    </ul>
+  </div>
 `;
